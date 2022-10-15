6341823a56789d53dcdfeb3117f4e87fdaeaa1fa
成瀬大毅 <naruse@mi.t.u-tokyo.ac.jp>
2022-10-15 15:41:43 +0900

    Initialize Frontend

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <p>テスト</p>
+    </>
+  );
+};
+
+export default Home;
