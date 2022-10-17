fd9df6f5350925323a8e0a839df4858f0ed13c92
axt-one <rwatanabe1623@gmail.com>
2022-10-17 22:29:42 +0900

    Add result download function

--- frontend/src/pages/generation-result.tsx
+++ frontend/src/pages/generation-result.tsx
@@ -1,25 +1,41 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { FiDownload } from "react-icons/fi";
 
 const WaitingPage: FC = () => {
   const [flag, setFlag] = useState<boolean>(false);
+  const [url, setURL] = useState<string>("");
+  // const [timer, setTimer] = useState<number>(0);
+  const timer = useRef<NodeJS.Timer>();
+
+  const getImage = async (url: string) => {
+    const data = await fetch(url);
+    const blob = await data.blob();
+    const objectURL = URL.createObjectURL(blob);
+    setURL(objectURL);
+    console.log(timer);
+    clearInterval(timer.current);
+  };
+
   useEffect(() => {
-    setTimeout(() => {
-      setFlag(true);
-    }, 2000);
+    console.log("useEffect");
+    timer.current = setInterval(() => {
+      getImage("https://images.dog.ceo/breeds/bouvier/n02106382_1365.jpg");
+    }, 500);
+    return () => clearInterval(timer.current);
   }, []);
+
   return (
     <>
-      {flag ? (
+      {url !== "" ? (
         <>
           <p>生成結果</p>
           <div>
-            <Image src="/WaitingIcon.gif" width={200} height={150} />
+            <Image src={url} width={256} height={256} />
           </div>
           <a
-            download="result-aicon-maker.gif"
-            href="/WaitingIcon.gif"
+            download="result-aicon-maker.jpg"
+            href={url}
             className="inline-block rounded border-2 border-blue-900 bg-blue-200 p-2"
           >
             ダウンロード
