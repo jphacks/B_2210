104465e5525b40d56dfce6deed6e72e96e114822
Sota <60801230+sotazum@users.noreply.github.com>
2022-10-25 00:08:58 +0900

    add analytics

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -5,6 +5,15 @@ import Image from "next/image";
 export const Header: FC = () => {
   return (
     <header className="sticky top-0 z-10 border-b bg-white">
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-8SWJSM71C2"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-8SWJSM71C2');
+      </script>
       <div className="mx-auto flex h-12 max-w-4xl items-center justify-between p-4">
         <Link href="/">
           <a>
